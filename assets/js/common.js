6e0c3c8f7af59200486245a4b58b773cfe61fb19
gdfqrqr <eunmikim0114@gmail.com>
2023-06-07 20:39:36 +0900

    230607_01

--- assets/js/common.js
+++ assets/js/common.js
@@ -82,25 +82,127 @@ $(function(){
 
     //footer menu
     let arr = [
-       {name: "DRAM"},
-       {name: "SSD"},
-       {name: "eStorage"},
-       {name: "오토모티브 메모리"},
-       {name: "Consumer Storage"},
-       {name: "프로세서"},
-       {name: "이미지센서"},
-       {name: "디스플레이 IC"},
-       {name: "보안 솔루션"}
+        {
+            name:'제품',
+            sub:[   
+                {name: "DRAM"},
+                {name: "SSD"},
+                {name: "eStorage"},
+                {name: "오토모티브 메모리"},
+                {name: "Consumer Storage"},
+                {name: "프로세서"},
+                {name: "이미지센서"},
+                {name: "디스플레이 IC"},
+                {name: "보안 솔루션"},
+                {name: "전력관리 IC"},
+                {name: "LED", arrow: true}
+            ]
+        },
+        {
+            name:'파운드리',
+            sub:[
+               {name: "개요"},
+               {name: "공정 기술"},
+               {name: "Advanced Package"},
+               {name: "SAFE™"},
+               {name: "제조"},
+               {name: "응용 서비스"},
+               {name: "Foundry Event"}
+            ]
+        },
+        {
+            name:'인사이트',
+            sub:[
+               {name: "5G"},
+               {name: "AI"},
+               {name: "빅 데이터"},
+               {name: "이미지 프로세싱"},
+               {name: "PIM"},
+               {name: "EUV"},
+               {name: "스마트폰"},
+               {name: "서버 & 네트워크"},
+               {name: "PC"},
+               {name: "TV & 게이밍"},
+               {name: "오토모티브"}
+            ]
+        },
+        {
+            name:'고객지원',
+            sub:[
+               {name: "개요"},
+               {name: "제품 찾기"},
+               {name: "기술 자료"},
+               {name: "Consumer Storage 지원"},
+               {name: "반도체 제조공정"},
+               {name: "용어 사전"},
+               {name: "글로벌 네트워크"},
+               {name: "B2B Workplace", arrow:true}
+            ]
+        },
+        {
+            name:'지속가능경영',
+            sub:[
+                {name:"주요 성과"},
+                {name:"환경"},
+                {name:"노동과 인권"},
+                {name:"사회공헌"},
+                {name:"지속가능한 공급망"}
+            ]
+        },
+        {
+            name:'회사 소개',
+            sub:[
+               {name:"사업영역"},
+               {name:"경영진"},
+               {name:"위치"},
+               {name:"연혁"},
+               {name:"채용"},
+               {name:"뉴스"},
+               {name:"이벤트"},
+               {name:"테크블로그"},
+               {name:"투자자 정보", arrow:true},
+               {name:"윤리", arrow:true},
+               {name:"반도체 뉴스룸", arrow:true}
+            ]
+        },
+        {
+            name:'윤리&준법경영',
+            sub:[
+               {name:"경영원칙", arrow:true},
+               {name:"부정제보", arrow:true},
+               {name:"법위반제보", arrow:true}
+            ]
+        }
     ]
 
     let footerHtml = ''
-   arr.forEach(el => {
+        arr.forEach(el => {
+            let innerTemplate = '';
 
-    const template = `<li class="quick-item"><a href="">${el.name}</a></li>`
-        footerHtml += template
-    })
+            
+            el.sub.forEach(e => {
 
-   $('.grid-inner .quick-list').html(footerHtml)
+                isArrow = (e.arrow)?`<img src="./assets/img/more-t3.svg" alt="" class="arrow-t2">`:'';
+
+                innerTemplate +=`
+                <ul class="quick-list">
+                    <li class="quick-item">
+                        <a href="">${e.name} ${isArrow}</a>
+                    </li>
+                </ul>`
+            })
+
+            let template = '';
+            template += `<div class="quick-wrap">
+            <h3 class="title-box">${el.name}</h3>
+            ${innerTemplate}
+            </div>
+            `;
+            
+            footerHtml += template;
+
+        })
+        $('.grid-inner').html(footerHtml)
    
 
    //scroll top
